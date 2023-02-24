c347d4cad542fcf32c6112aae0cf6d3c18690be9
Dhwanil Patel <dpatel124@nait.ca>
2023-02-24 15:47:51 -0700

    updated Stream Demo

--- dmit2015-azure-webapp/src/main/java/dmit2015/model/CanadianPersonalIncomeTaxManager.java
+++ dmit2015-azure-webapp/src/main/java/dmit2015/model/CanadianPersonalIncomeTaxManager.java
@@ -4,9 +4,29 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CanadianPersonalIncomeTaxManager {
+
+    public List<String> readAllLinesFromCsvFile(String filePathString) {
+        List<String> allLines = new ArrayList<>();
+
+        try {
+            Path csvPath = Path.of(Thread
+                    .currentThread()
+                    .getContextClassLoader()
+                    .getResource(filePathString)
+                    .toURI());
+            allLines = Files.readAllLines(csvPath);
+            // Skip the first line using Streams
+            allLines = allLines.stream().skip(1).toList();
+        } catch (URISyntaxException | IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return allLines;
+    }
     public static void main(String[] args) {
         try {
             Path csvPath = Path.of(Thread.currentThread().getContextClassLoader().getResource("data/CanadianPersonalIncomeTaxRates.csv").toURI());
